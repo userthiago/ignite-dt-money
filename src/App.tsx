1cef4b0637076258105daf4e2bf883696d3eb8fd
Thiago Santos <tisantos.ext@vr.com.br>
2021-03-17 09:08:08 -0300

    style: Adicionando estilos globais.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import GlobalStyle from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello dt.money</h1>
+      <GlobalStyle />
     </div>
   );
 }
