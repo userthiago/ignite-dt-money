8e54b9f425d01bd5697d5036817cb5413cc40734
Thiago <thiagosantosprofissional@gmail.com>
2021-03-20 17:01:47 -0300

    feat: Criando estrutura base do modal de transação.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import logoImg from '../../assets/logo.svg';
 import { HeaderContainer } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
   return (
     <HeaderContainer>
       <div className="header__content">
         <img src={logoImg} alt="dt money logo"/>
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
       </div>
